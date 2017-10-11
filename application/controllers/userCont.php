d26811829bda86526c7758099afc8e05589add06
huamgg <fahmisalmin@gmail.com>
2017-10-11 14:54:49 +0700

    first
    first blood

---
+++ application/controllers/userCont.php
@@ -0,0 +1,27 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+class userCont extends CI_Controller {
+
+	//set as default
+	public function __construct(){
+        
+        parent::__construct();
+        if ($_SESSION['user_logged'] == FALSE){
+            $this->session->set_flashdata("error", "Please login first to view this page!! ");
+            redirect("authCont/loginView");
+        }
+    }
+
+    //for get userpage after user login
+    public function userpage(){
+
+        if ($_SESSION['user_logged'] == FALSE){
+            $this->session->set_flashdata("error", "Please login first to view this page!! ");
+            redirect("authCont/login");
+        }
+        
+            $this->load->view('userpageView', 'refresh');
+        
+    }
+}
