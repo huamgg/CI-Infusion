3f077319bbe969c822adc051d5f9c66082a05d13
Ryan Henry <ryan.henry20@hotmail.com>
2017-10-13 17:18:31 +0700

    sdsd
    commit

--- application/models/Getter.php
+++ application/models/Getter.php
@@ -3,13 +3,15 @@ class Getter extends CI_Model{
 
     //this is for get dashboard view
     function get_dash_content()
-    {        
+    {
         $query = $this->db->query("SELECT c.id, c.type, c.campaign_name, l.label_name, c.status
         FROM `campaigns` c
         LEFT JOIN labels l ON c.label_id = l.id"
         );
 
         return $query->result();
-   
+
     }
+
+
 }
